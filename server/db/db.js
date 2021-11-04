2e86903968b65575827f4d975cf5b42f555b86be
Test <test@Kevz-PC.local>
2021-11-04 14:35:18 -0400

    Add config file import

--- server/db/db.js
+++ server/db/db.js
@@ -1,3 +1,5 @@
+require('dotenv').config({path: 'config.env'});
+
 const Sequelize = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
