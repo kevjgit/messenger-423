00eb3bfe1bb71f621362a7d8ba08fd9c00968192
Test <test@Kevz-PC.local>
2021-11-05 13:14:46 -0400

    Add helpful info to model files

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -7,6 +7,9 @@ const Conversation = db.define("conversation", {});
 // find conversation given two user Ids
 
 Conversation.findConversation = async function (user1Id, user2Id) {
+  /**
+   * Op.or: Select * from conversation where user1Id in either (user1Id or user2Id) and user2Id in either (user1Id or user2Id)
+   */
   const conversation = await Conversation.findOne({
     where: {
       user1Id: {
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,11 +4,19 @@ const Message = require("./message");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+/**
+ * Documentation:
+ * belongsTo - add a foreign key and singular association mixins to the source.
+ * hasMany - adds a foreign key to target and plural association mixins to the source.
+ * Conversation has a foreign key to User
+ * Message has a foreign key (conversationId) to Conversation 
+ */
+
+User.hasMany(Conversation); //Conversation has a reference to User in a 1-many relationship
+Conversation.belongsTo(User, { as: "user1" }); //Conversation has a reference to User
+Conversation.belongsTo(User, { as: "user2" }); 
 Message.belongsTo(Conversation);
-Conversation.hasMany(Message);
+Conversation.hasMany(Message); //Message has a reference to Conversation
 
 module.exports = {
   User,
