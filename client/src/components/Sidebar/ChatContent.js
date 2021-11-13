7153d43c82ff015367602513c0988940e3e503f7
Test <test@Kevz-PC.local>
2021-11-12 21:22:40 -0500

    Update styling to reflect changes to sidebar content in design doc

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -17,7 +16,28 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 12,
     color: "#9CADC8",
     letterSpacing: -0.17,
+    fontWeight: "bold",
   },
+  previewTextUnseen:{
+    fontSize: 12,
+    letterSpacing: -0.17,
+    fontWeight: "bold",
+    color: 'black'
+  },
+
+  unseenMessages: {
+    display: 'inline-block',
+    backgroundColor: '#3A8DFF',
+    borderRadius: '40px', 
+    padding: '0 5px',
+    color: 'white',
+    fontSize: '10px',
+    fontWeight: 'bold',
+    alignSelf: 'flex-end', 
+    marginRight: '20px', 
+    marginTop: '15px', 
+    position: 'absolute'
+  }
 }));
 
 const ChatContent = (props) => {
@@ -25,14 +45,16 @@ const ChatContent = (props) => {
 
   const { conversation } = props;
   const { latestMessageText, otherUser } = conversation;
+  const messageLen = conversation.messages.filter(message => !message.seen && message.senderId === otherUser.id).length;
 
   return (
     <Box className={classes.root}>
-      <Box>
+      <Box style={{display: 'flex', flexDirection: 'column', width: '100%'}}>
         <Typography className={classes.username}>
           {otherUser.username}
         </Typography>
-        <Typography className={classes.previewText}>
+        <Typography className={classes.unseenMessages}> {messageLen > 0 ? messageLen : ''} </Typography>
+        <Typography className={messageLen === 0 ? classes.previewText : classes.previewTextUnseen}>
           {latestMessageText}
         </Typography>
       </Box>
