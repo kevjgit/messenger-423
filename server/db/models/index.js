ef6751a5f41eef02ae52ea2c4945077e9263a949
Test <test@Kevz-PC.local>
2021-11-12 21:38:32 -0500

    remove comments

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,19 +4,12 @@ const Message = require("./message");
 
 // associations
 
-/**
- * Documentation:
- * belongsTo - add a foreign key and singular association mixins to the source.
- * hasMany - adds a foreign key to target and plural association mixins to the source.
- * Conversation has a foreign key to User
- * Message has a foreign key (conversationId) to Conversation 
- */
 
-User.hasMany(Conversation); //Conversation has a reference to User in a 1-many relationship
-Conversation.belongsTo(User, { as: "user1" }); //Conversation has a reference to User
+User.hasMany(Conversation); 
+Conversation.belongsTo(User, { as: "user1" }); 
 Conversation.belongsTo(User, { as: "user2" }); 
 Message.belongsTo(Conversation);
-Conversation.hasMany(Message); //Message has a reference to Conversation
+Conversation.hasMany(Message); 
 
 module.exports = {
   User,
