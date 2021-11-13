229bcf6936d4a1955f2c8a69c699cc38623e3b77
Test <test@Kevz-PC.local>
2021-11-12 21:01:22 -0500

    Get the last message by sender and set the photo to that message.

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -4,18 +4,20 @@ import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
 const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
+  const { messages, otherUser, userId} = props;
+  const lastMessageBySender = messages ? messages.slice().reverse().find((message) => message.seen && message.senderId === userId) : ''
 
   return (
     <Box>
-      {messages.map((message) => {
+      {messages.map((message) => { 
         const time = moment(message.createdAt).format("h:mm");
-
-        return message.senderId === userId ? (
+        if (message.senderId === userId){
+          return lastMessageBySender && lastMessageBySender.id === message.id ? 
+          <SenderBubble key={message.id} text={message.text} time={time} seenPhoto={otherUser.photoUrl} /> : 
           <SenderBubble key={message.id} text={message.text} time={time} />
-        ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
-        );
+        }else{
+          return <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+        }
       })}
     </Box>
   );
