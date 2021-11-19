b8d3eacc2166f20eb3cb8c60d4b1df85af59f39e
Test <test@Kevz-PC.local>
2021-11-18 20:05:14 -0500

    Enable lookup of UserConversation by passing the senderId and conversationId from a message.

--- server/db/models/user-conversation.js
+++ server/db/models/user-conversation.js
@@ -3,4 +3,18 @@ const db = require("../db");
 
 const UserConversation = db.define("user-conversation", {} );
 
+UserConversation.findUserConversation = async function (userId, conversationId) {
+   
+    const userConversation = await UserConversation.findOne({
+      where: {
+        [Op.and]: [
+            {userId: userId},
+            {conversationId: conversationId}
+        ]
+      }
+    });
+  
+    return userConversation;
+  };
+
 module.exports = UserConversation;
